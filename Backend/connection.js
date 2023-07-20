dfd42249235e6e4a40959e255e37badd4cd87ed4
m-triple-m <triplem656@gmail.com>
2023-07-20 19:23:46 +0530

    ..

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const url='mongodb+srv://Hema:rathour@cluster0.3ptbhma.mongodb.net/personalchef?retryWrites=true&w=majority';
-const url='mongodb+srv://mmm:mmm@cluster0.gvyon.mongodb.net/chef?retryWrites=true&w=majority';
+const url='mongodb+srv://Hema:rathour@cluster0.3ptbhma.mongodb.net/personalchef?retryWrites=true&w=majority';
+// const url='mongodb+srv://mmm:mmm@cluster0.gvyon.mongodb.net/chef?retryWrites=true&w=majority';
 mongoose.connect(url)
 .then((result) => {
     console.log('database connected');
